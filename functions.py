b65e823b4c347d71ff3b09b72d31da7b51765456
Logan Nance <ldnance10@gmail.com>
2022-11-15 19:00:50 -0700

    Get unit

--- functions.py
+++ functions.py
@@ -4,8 +4,10 @@
 # get user input for the length to convert
 length = input("What is the length? ")
 print(length)
-# get the unit from the user
 
+# get the unit from the user
+unit = input("What unit is the length? ")
+print(unit)
 
 # convert the input into the desired units
 
