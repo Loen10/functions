f98792724a27541421fc51f0a953e144efa4f499
Logan Nance <ldnance10@gmail.com>
2022-11-15 19:12:23 -0700

    Conditional stmt for unit

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@
 # Logan Nance
 
 # get user input for the length to convert
-length = input("What is the length? ")
+length = float(input("What is the length? "))
 print(length)
 
 # get the unit from the user
@@ -10,7 +10,12 @@ unit = input("What unit is the length? ")
 print(unit)
 
 # convert the input into the desired units
+# to convert in to mm: in * 25.4
 
+if unit == "in":
+    converted_length = length * 25.4
+elif unit == "mm":
+    converted_length = length / 25.4
 
 # output the answer to the user
-
+print(converted_length)
