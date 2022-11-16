81ce095bce6427b8d56bd330b724050f39adaacf
Logan Nance <ldnance10@gmail.com>
2022-11-15 18:58:14 -0700

    Get user input

---
+++ functions.py
@@ -0,0 +1,14 @@
+# Conversion Calculator
+# Logan Nance
+
+# get user input for the length to convert
+length = input("What is the length? ")
+print(length)
+# get the unit from the user
+
+
+# convert the input into the desired units
+
+
+# output the answer to the user
+
