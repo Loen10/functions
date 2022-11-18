e4e2786af60aded58425cad64df5a3df5d3293cd
Logan Nance <ldnance10@gmail.com>
2022-11-17 18:31:16 -0700

    Error check for unit and number

--- functions.py
+++ functions.py
@@ -1,24 +1,33 @@
 # Conversion Calculator
 # Logan Nance
 
-# get user input for the length to convert
-length = float(input("What is the length? "))
-print(length)
+while True:
+    # get user input for the length to convert
+    length = input("What is the length? ")
+    if length.isdecimal():
+        length = float(length)
+    else:
+        print("Your length sucks. Try again")
+        continue
 
-# get the unit from the user
-unit = input("What unit is the length? ")
-print(unit)
+    print(length)
 
-# convert the input into the desired units
-# to convert in to mm: in * 25.4
-if unit == "in":
-    converted_length = length * 25.4
-    converted_unit = "mm"
-elif unit == "mm":
-    converted_length = length / 25.4
-    converted_unit = "in"
-else:
-    print("Your unit sucks. Try again")
+    # get the unit from the user
+    unit = input("What unit is the length? ")
+    print(unit)
+
+    # convert the input into the desired units
+    # to convert in to mm: in * 25.4
+    if unit == "in":
+        converted_length = length * 25.4
+        converted_unit = "mm"
+        break
+    elif unit == "mm":
+        converted_length = length / 25.4
+        converted_unit = "in"
+        break
+    else:
+        print("Your unit sucks. Try again")
 
 # output the answer to the user
 print(converted_length, converted_unit)
