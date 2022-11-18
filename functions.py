a59ea1c67a56083b910d41b9c3fd9d4d427c6414
Logan Nance <ldnance10@gmail.com>
2022-11-17 18:57:54 -0700

    parsing function

--- functions.py
+++ functions.py
@@ -1,33 +1,25 @@
 # Conversion Calculator
 # Logan Nance
 
-while True:
-    # get user input for the length to convert
-    length = input("What is the length? ")
-    if length.isdecimal():
-        length = float(length)
-    else:
-        print("Your length sucks. Try again")
-        continue
-
-    print(length)
-
-    # get the unit from the user
-    unit = input("What unit is the length? ")
-    print(unit)
+def parse_length_with_unit(length_with_unit: str) -> float:
+    (length, unit) = length_with_unit.split()
+    length = float(length)
 
     # convert the input into the desired units
-    # to convert in to mm: in * 25.4
-    if unit == "in":
-        converted_length = length * 25.4
-        converted_unit = "mm"
-        break
-    elif unit == "mm":
-        converted_length = length / 25.4
-        converted_unit = "in"
-        break
-    else:
-        print("Your unit sucks. Try again")
+    # to convert in to mm: in * 25.4    
+    while True:
+        if unit == "in":
+            converted_length = length * 25.4
+            converted_unit = "mm"
+            break
+        elif unit == "mm":
+            converted_length = length / 25.4
+            converted_unit = "in"
+            break
+        else:
+            print("Your unit sucks. Try again")
+    
+    return converted_length
 
-# output the answer to the user
-print(converted_length, converted_unit)
+# get user input for the length to convert
+print(parse_length_with_unit(input("What is the length and unit? ")))
